20ff6e82dd73cc9b66198ba98c249010b1853717
valter <valter.silva@see.pb.gov.br>
2025-03-19 10:31:12 -0300

    Commit Salvar dados da tela para o banco de dados

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from app_cad_usuarios import views
 
 urlpatterns = [
+    # rota, view responsavel, nome de referencia
+
     path('', views.home, name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/',views.usuarios,name='listagem_usurios')
+
 ]
